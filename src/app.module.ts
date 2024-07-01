8b0d752184e8566f40f7f5e606433b120a56e269
Mehmet Emin Kartal <mehmet@appac.ltd>
2024-07-02 02:28:51 +0300

    feat: Add JWTWalletModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JWTWalletModule } from "jwtwallet-nestjs";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
@@ -9,6 +10,16 @@ import { ConfigModule } from "@nestjs/config";
       envFilePath: [".env.local", ".env.default"],
       isGlobal: false,
       ignoreEnvVars: true
+    }),
+    JWTWalletModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        privateKey: configService.get("JWT_PRIVATE"),
+        issuer: configService.get("JWT_ISSUER"),
+        devPublicKey: configService.get("JWT_MOCK_PUBLIC"),
+        devPort: configService.get("PORT")
+      }),
+      inject: [ConfigService]
     })
   ],
   controllers: [AppController],
