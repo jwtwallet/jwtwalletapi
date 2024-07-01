df6ef60005613a72b9c4a9b858cae4a58922238a
Mehmet Emin Kartal <mehmet@appac.ltd>
2024-07-01 15:30:43 +0300

    feat: Added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env.local", ".env.default"],
+      isGlobal: false,
+      ignoreEnvVars: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
